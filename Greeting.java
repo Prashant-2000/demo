7ee6dcd6d4becaa43d239e139ae4f7a3106b2a4f
Prashant <prashantgogde1006@gmail.com>
2024-07-31 00:33:32 +0530

    second commit

--- Greeting.java
+++ Greeting.java
@@ -1,6 +1,6 @@
 public class Greeting {
     public static void main(String[] args) {
         // Print the greeting message to the console
-        System.out.println("Hello, good morning!");
+        System.out.println("Hello, good morning \nHave a good day dear...!");
     }
 }
