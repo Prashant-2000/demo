57a594f8d0209388564d286a479c81f8510b0765
Prashant <prashantgogde1006@gmail.com>
2024-07-31 00:30:40 +0530

    first commit

---
+++ Greeting.java
@@ -0,0 +1,6 @@
+public class Greeting {
+    public static void main(String[] args) {
+        // Print the greeting message to the console
+        System.out.println("Hello, good morning!");
+    }
+}
