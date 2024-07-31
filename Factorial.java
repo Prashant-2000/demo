f21bb5645007b2fa29fca67a2f39bb2f9335b1ba
Prashant gogde <prashantgogde1006@gmail.com>
2024-07-31 09:15:53 +0530

    first commit

---
+++ Factorial.java
@@ -0,0 +1,27 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int number = 5; // example number
+        int factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+
+    /**
+     * Calculates the factorial of a given number.
+     * 
+     * @param number the number to calculate the factorial for
+     * @return the factorial of the given number
+     */
+    public static int calculateFactorial(int number) {
+        if (number < 0) {
+            throw new ArithmeticException("Factorial is not defined for negative numbers");
+        } else if (number == 0 || number == 1) {
+            return 1;
+        } else {
+            int result = 1;
+            for (int i = 2; i <= number; i++) {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
